1d56a791179aeb241107e360b10ada8b8f0855ed
MarvNet <marvin.kai@gmx.de>
2017-10-31 09:39:30 +0100

    Added basic virtual machine

---
+++ vagrant/forum/forum.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f7db1b1a73e43f6da1c8bf613586c92990cd861b043ef981536ffa635166912
+size 134
---
+++ vagrant/tournament/tournament.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10e9f11c87c9c5f1f49a62c2058752d98012d24dc9268fc1b6283f87e05fe1d5
+size 270
