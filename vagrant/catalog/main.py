37c48ffbd8121ec8a70313e4266376e5c9f2f163
MarvNet <marvin.kai@gmx.de>
2017-10-31 12:03:04 +0100

    Added basic flask structure

--- vagrant/catalog/main.py
+++ vagrant/catalog/main.py
@@ -5,3 +5,15 @@ import json
 from oauth2client import client, crypt
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+# Initialize Flask-App
+app = Flask(__name__)
+
+
+
+
+# Run flask app
+if __name__ == '__main__':
+    # app.debug = True
+    app.secret_key = "not_very_secretive"
+    app.run(host='0.0.0.0', port=8080)
